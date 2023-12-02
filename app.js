c0ab2cf6479db32ed7634ba33bd24e87c611a12b
unknown <jayeshgupta2109@gmail.com>
2023-12-02 10:42:16 +0530

    added JSX

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
 import React from "react"
 import ReactDOM  from "react-dom/client";
 
-/*  <script>
-const heading=document.createElement("h1");
-heading.innerHTML="Hello javascript!";
-const root=document.getElementById("root");
-root.appendChild(heading);
-</script> */
-//nested elements
-/*
-<div id="parent">
-    <div id="child">
-        <h1></h1>
-        <h2></h2>
+// JSX ==> React.createElement ==> ReactElement - obj ==> HTMLELEment
+const jsxHeading=<h1 id="heading" className="heading">Hello JSX</h1>;
+
+const Title=()=>(
+    <h1>This is Title</h1>
+)
+const Heading=()=>(
+    <div>
+        {Title()}
+        <Title/>
+        <Title></Title>
+        <h1>Hello from React Component</h1>
     </div>
-    <div id="child2">
-        <h1></h1>
-        <h2></h2>
-    </div>
-</div>
+     );
+
+const Assignment=()=>(
+<div className="title">
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <h3>H3</h3>
+</div>);
 
 
-*/
-const nested=React.createElement("div",{"id":"parent"},
-    React.createElement("div",{"id":"child"},
-        React.createElement("h1",{},"Hello")));
-//siblings
-const siblings=React.createElement("div",{"id":"parent"},
-    React.createElement("div",{"id":"child"},
-        [React.createElement("h1",{},"h1"),React.createElement("h2",{},"h2")]));
-
-const nestedDiv=React.createElement("div",{"id":"parent"},
-        [React.createElement("div",{"id":"child"},
-        [React.createElement("h1",{},"h1"),React.createElement("h2",{},"h2")]),
-        React.createElement("div",{"id":"child2"},
-        [React.createElement("h1",{},"h1"),React.createElement("h2",{},"h2")])]);
-        
-console.log(nested);
-
-const heading=React.createElement("h1",{"id":"heading"},"Hello React!");// returns object
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(nestedDiv); // converting object into h1 tag and putting it in dom
+const root=ReactDOM.createRoot(document.getElementById("root"))
+//root.render(<Heading/>);
+root.render(<Assignment/>);
---
+++ ep1.js
@@ -0,0 +1,43 @@
+import React from "react"
+import ReactDOM  from "react-dom/client";
+
+/*  <script>
+const heading=document.createElement("h1");
+heading.innerHTML="Hello javascript!";
+const root=document.getElementById("root");
+root.appendChild(heading);
+</script> */
+//nested elements
+/*
+<div id="parent">
+    <div id="child">
+        <h1></h1>
+        <h2></h2>
+    </div>
+    <div id="child2">
+        <h1></h1>
+        <h2></h2>
+    </div>
+</div>
+
+
+*/
+const nested=React.createElement("div",{"id":"parent"},
+    React.createElement("div",{"id":"child"},
+        React.createElement("h1",{},"Hello")));
+//siblings
+const siblings=React.createElement("div",{"id":"parent"},
+    React.createElement("div",{"id":"child"},
+        [React.createElement("h1",{},"h1"),React.createElement("h2",{},"h2")]));
+
+const nestedDiv=React.createElement("div",{"id":"parent"},
+        [React.createElement("div",{"id":"child"},
+        [React.createElement("h1",{},"h1"),React.createElement("h2",{},"h2")]),
+        React.createElement("div",{"id":"child2"},
+        [React.createElement("h1",{},"h1"),React.createElement("h2",{},"h2")])]);
+        
+console.log(nested);
+
+const heading=React.createElement("h1",{"id":"heading"},"Hello React!");// returns object
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(nestedDiv); // converting object into h1 tag and putting it in dom
